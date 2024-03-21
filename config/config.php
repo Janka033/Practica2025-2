a7e44a380ff641f3e9199e6b98913609a4933aeb
Angel Sifuentes <angelsifuentes2580@gmail.com>
2024-03-21 18:59:32 -0500

    Version 1

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+define('ADMIN', 'admin');
+define('RUTA_PRINCIPAL', 'http://localhost/reservas/');
+define('RUTA_ADMIN', RUTA_PRINCIPAL . ADMIN . '/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DATABASE', 'reservas');
+define('CHARSET', 'charset=utf8');
+define('TITLE', 'VIDA INFORMATICO');
+//PAYPAL
+define('MONEDA_PAYPAL', 'USD');
+define('CLIENTE_ID', 'AW2tkMdo0e0YQTtRqZqfXIRgb-H6_1ioRMdFgiuleCLO6DNfwCsTnvzJXDqfy6_6HfAaBfw3mTlnFhik');
+//MERCADO PAGO
+define('MONEDA_MP', 'USD');
+define('PUBLIC_KEY', 'TEST-0968492a-651b-40bd-a5db-76d3f2319102');
+define('ACCESS_TOKEN', 'TEST-876394235929292-011613-acc505692067d6219c58f6fc09163887-429694665');
+//SMTP
+define('HOST_SMTP', 'smtp.gmail.com');
+define('USER_SMTP', 'lovenaju2@gmail.com');
+define('CLAVE_SMTP', 'dqfpxqgoqrwoxrah');
+define('PUERTO_SMTP', 465);
+
+?>
